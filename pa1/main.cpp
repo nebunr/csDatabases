ac477b6e7fe737c4961a3734c7ff28959ceb3c92
Benjamin Estela <benjaminestela@nevada.unr.edu>
2020-02-03 13:32:44 -0800

    Update for basic file io

--- pa1/main.cpp
+++ pa1/main.cpp
@@ -1,8 +1,26 @@
+//make; ./pa1 PA1_test.sql
 #include <iostream>
 #include <fstream>
 using namespace std;
-int main()
+int main(int argc, char** argv)
 {
-    cout << "test" << endl;
+    try
+    {
+        if(!argv[1])
+        {
+            throw "No filename found.";
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    ifstream sqlFile;
+    sqlFile.open(argv[1]);
+    
+    cout << "testing: " << argv[1] << endl;
+    
+    sqlFile.close();
+    
     return 0;
 }
